eabc95d4b9b1e295cf89d5f8b702b6cb55a38593
ZdapX <stokmlmurmer150@gmail.com>
2026-04-01 20:37:39 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,12 +6,16 @@ const fs = require('fs');
 const app = express();
 app.use(cors());
 
-// Endpoint untuk mengambil data font
 app.get('/ambil-data-font', (req, res) => {
-    const filePath = path.join(process.cwd(), 'public', 'fonts.json');
-    const jsonData = fs.readFileSync(filePath, 'utf-8');
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).send(jsonData);
+    try {
+        // process.cwd() adalah cara terbaik di Vercel untuk mencari root folder
+        const filePath = path.join(process.cwd(), 'public', 'fonts.json');
+        const jsonData = fs.readFileSync(filePath, 'utf-8');
+        res.setHeader('Content-Type', 'application/json');
+        res.send(jsonData);
+    } catch (error) {
+        res.status(500).json({ error: "File fonts.json tidak ditemukan" });
+    }
 });
 
 module.exports = app;
