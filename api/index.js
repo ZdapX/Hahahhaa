3bac26cd2ccf7eb0f7e70de8830ea68b445ed315
ZdapX <stokmlmurmer150@gmail.com>
2026-04-01 21:09:44 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,20 +1,7 @@
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
-const fs = require('fs');
-
 const app = express();
-app.use(cors());
 
-app.get('/ambil-data-font', (req, res) => {
-    try {
-        const filePath = path.join(process.cwd(), 'public', 'fonts.json');
-        const jsonData = fs.readFileSync(filePath, 'utf-8');
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).send(jsonData);
-    } catch (error) {
-        res.status(500).json({ error: "Data tidak ditemukan" });
-    }
-});
+app.use(express.static(path.join(__dirname, '../public')));
 
 module.exports = app;
